5cbfdf666be74c08bba95c82fbba1bcba43bafda
kwagdaeyong <dayong7387@hanmail.net>
2017-09-13 13:39:29 +0900

    release retry

--- src/main/java/com/example/demo/DanymallApplication.java
+++ src/main/java/com/example/demo/DanymallApplication.java
@@ -1,11 +1,19 @@
 package com.example.demo;
 
+import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DanymallApplication {
+public class DanymallApplication extends SpringBootServletInitializer {
 	private static final String PROPERTIES = "spring.config.location=classpath:/google.properties";
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(DanymallApplication.class)
 				.properties(PROPERTIES)
